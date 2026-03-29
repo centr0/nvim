cd7538da3916e1c56269cd3fa33eb771a915eba6
centro <centroscape@gmail.com>
2026-03-29 00:00:49 -0700

    update

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,4 +2,4 @@ vim.opt.guicursor = "n-v-i-c:block-Cursor"
 vim.opt.clipboard = "unnamedplus"
 vim.opt.smoothscroll = false
 -- vim.opt.textwidth = 80
--- vim.opt.colorcolumn = "80"
+vim.opt.colorcolumn = "88"
